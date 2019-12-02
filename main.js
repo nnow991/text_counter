74fb9b004c5af063cdbb105d9da792e77509b7d0
Natalia <nnowak91@gmail.com>
2019-12-02 10:50:02 +0100

    text_counter edited2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function myCounter(){
 let getText= document.getElementById("textarea");
-let textLength= (getText.value.length);
+let textLength= (getText.value.length + 1);
 document.getElementById("output").innerHTML="You typed " +
 textLength + " characters";
 }
