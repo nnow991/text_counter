d35307d85b1ac1698e549b297b0bd6b3b4657905
Natalia <nnowak91@gmail.com>
2019-12-07 03:53:27 +0100

    word_counter

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+
+
 function myCounter(){
-let getText= document.getElementById("textarea");
+    let getText= document.getElementById("textarea");
 let textLength= (getText.value.length + 1);
-document.getElementById("output").innerHTML="You typed " +
-textLength + " characters";
+    let words = document.getElementById("textarea").value;
+
+    words=words.match( /\w+/g);
+    words= words.length
+
+    document.getElementById("showwords").innerHTML= "You typed " +
+    textLength + " characters" + "</br>" + " and " + words + " words" ;
+
 }
